fc3b61a7b277cd7fb3b2b21a5e1a352d0b5959dc
winterkid <137682088+winterkid2000@users.noreply.github.com>
2025-05-28 10:20:21 +0900

    Update NIFTI_2_STL.py

--- Step 2/NIFTI_2_STL.py
+++ Step 2/NIFTI_2_STL.py
@@ -6,15 +6,18 @@ from stl import mesh
 from tqdm import tqdm
 
 def nifti_to_stl(nifti_path: str, stl_path: str, threshold: float = 0):
+    ##nibabel로 NIfTI 마스크가 갖고 있는 데이터부터 추출
     try:
         img = nib.load(nifti_path)
         data = img.get_fdata()
         affine = img.affine
 
+        ##마스크 영역의 임계값을 정해서 어떤 느낌으로 갈지 결정 
         binary_mask = data > threshold
         if not np.any(binary_mask):
             raise ValueError("Threshold보다 큰 값이 없는뎅... 마스크가 비어 있는 거 같애")
-
+            
+        ##Marching Cubes 알고리즘 적용
         verts, faces, _, _ = measure.marching_cubes(binary_mask.astype(np.float32), level=0)
 
         verts_homogeneous = np.c_[verts, np.ones(len(verts))]
@@ -32,7 +35,8 @@ def nifti_to_stl(nifti_path: str, stl_path: str, threshold: float = 0):
     except Exception as e:
         print(f"Blast {nifti_path} 변환 오류: {e}")
         return False
-
+        
+##위 nifti_to_stl 함수가 돌아가기 위해 어떤 방식일지 정의 
 def convert_all_nii_to_stl_simple(nii_base_path: str, stl_output_path: str, threshold: float = 0):
     failed = []
 
